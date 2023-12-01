3c986a4388eabaf78e40f5b9d8cb55120e7ef2b4
Cristopher Ferrari <ferraricristopher93@gmail.com>
2023-12-01 15:06:14 +0100

    Creare l’array di oggetti con le informazioni.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,41 @@
-/*
-Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
-    MILESTONE 0:
-Creare l’array di oggetti con le informazioni fornite.
+
+//Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+    //MILESTONE 0:
+//Creare l’array di oggetti con le informazioni fornite.
+    let team = [
+        {
+            'name' : 'Wayne Barnett',
+            'role' : 'Founder & CEO',
+            'img': 'wayne-barnett-founder-ceo.jpg'
+        },
+        {
+            'name': 'Angela Caroll',
+            'role': 'Chief Editor',
+            'img': 'angela-caroll-chief-editor.jpg'
+        },
+        {
+            'name': 'Walter Gordon',
+            'role': 'Office Manager',
+            'img': 'walter-gordon-office-manager.jpg'
+        },
+        {
+            'name': 'Angela Lopez',
+            'role': 'Social Media Manager',
+            'img': 'angela-lopez-social-media-manager.jpg'
+        },
+        {
+            'name': 'Scott Estrada',
+            'role': 'Developer',
+            'img': 'scott-estrada-developer.jpg'
+        },
+        {
+            'name': 'Barbara Ramos',
+            'role': 'Graphic Designer',
+            'img': 'barbara-ramos-graphic-designer.jpg'
+        }
+
+    ]
+
     MILESTONE 1:
 Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
     MILESTONE 2:
@@ -10,4 +44,5 @@ Stampare le stesse informazioni su DOM sotto forma di stringhe
 Trasformare la stringa foto in una immagine effettiva
     BONUS 2:
 Organizzare i singoli membri in card / schede
-*/
+
+
